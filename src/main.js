f9ebcb76cec04b1db89726bf58f1a5129d89b232
Carolyn <2535806990@qq.com>
2023-03-03 00:29:40 +0800

    引入axios,elementui

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+Vue.use(Element)
+
+import axios from 'axios'
+Vue.prototype.$axios = axios //
+
 createApp(App).use(store).use(router).mount('#app')
