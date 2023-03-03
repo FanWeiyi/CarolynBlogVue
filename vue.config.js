7772838674b83a525b7eb6d6ecec6eae61549f6f
Carolyn <2535806990@qq.com>
2023-03-03 23:53:32 +0800

    部署修改

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {    
-    resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },    
-    module: {        
-      rules: [    
-        {    
-          test: /\.tsx?$/,    
-          loader: 'ts-loader',    
-          exclude: /node_modules/,    
-          options: {
-            appendTsSuffixTo: [/\.vue$/],    
-          }    
-        }        
-      ]    
-    }    
-  }
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   configureWebpack: {    
+//     resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },    
+//     module: {        
+//       rules: [    
+//         {    
+//           test: /\.tsx?$/,    
+//           loader: 'ts-loader',    
+//           exclude: /node_modules/,    
+//           options: {
+//             appendTsSuffixTo: [/\.vue$/],    
+//           }    
+//         }        
+//       ]    
+//     }    
+//   }
+// })
+module.exports = {
+  publicPath: '/'
+}
+
